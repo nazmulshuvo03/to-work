368b5e57dd970315c1c0be76b1aba4f2209bdd1a
nazmulshuvo03 <nazmulshuvo03@gmail.com>
2020-09-17 01:27:04 +0600

    firebase and facebook login connected

--- screen/StartupScreen.js
+++ screen/StartupScreen.js
@@ -1,5 +1,18 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import * as firebase from "firebase";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyD6LK_y37jl-dbKxkBAAl5uuoGT-yBbupU",
+    authDomain: "to-work-c78ce.firebaseapp.com",
+    databaseURL: "https://to-work-c78ce.firebaseio.com",
+    projectId: "to-work-c78ce",
+    storageBucket: "to-work-c78ce.appspot.com",
+    messagingSenderId: "243383915564",
+    appId: "1:243383915564:web:abdaa422ee56620c476d89",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const StartupScreen = () => {
     return (
